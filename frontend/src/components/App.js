893d81740ec6c2b09ebe412ecaa2ed3948f491de
David Johnston <david@blacksheepcode.com>
2019-03-16 10:49:13 +1100

    Add CRA
     - Add redux, axios, material-ui
     - Setup file structure

---
+++ frontend/src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+class App extends Component {
+  render() {
+    return (
+
+      <CssBaseline>
+        <div className="App">
+
+          hello world
+        </div>
+      </CssBaseline>
+    );
+  }
+}
+
+export default App;

dc7dcf5e9eb35319ddd0c99e054603856d276e2d
David Johnston <david@blacksheepcode.com>
2019-03-16 11:15:51 +1100

    Add dummy service

---
+++ frontend/src/services/ProductService.js
@@ -0,0 +1,8 @@
+
+// Just mock this for now
+export async function fetchAllProducts() {
+    return [
+        { id: "1", name: "Sample1", priceUsd: 1000 },
+        { id: "2", name: "Sample2", priceUsd: 2000 }
+    ]
+}
